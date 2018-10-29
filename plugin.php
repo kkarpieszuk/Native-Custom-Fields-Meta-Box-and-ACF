a7269a689ea754279547083918bdc096e966da86
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2018-10-29 11:29:48 +0100

    Updated plugin header

--- plugin.php
+++ plugin.php
@@ -1,8 +1,12 @@
 <?php
-/**
+/*
 Plugin Name: Native Custom Fields Meta Box and ACF
 Description: Show WordPress native meta box for custom fields when Advanced Custom Fields plugin is active
 Author: Konrad Karpieszuk
+Author URI: http://muzungu.pl
+Version: 1.0
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 add_filter( 'acf/settings/remove_wp_meta_box', '__return_false' );
